519d2d9132e5825c66739e8dd9fff6b7a39b0bb1
unknown <dandalofi09@gmail.com>
2026-06-25 17:16:58 -0300

    ajuste deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+//import { defineConfig } from 'astro/config';
+//import mdx from '@astrojs/mdx';
+//import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
-export default defineConfig({
-  integrations: [mdx(), sitemap()]
-});
+//export default defineConfig({
+//  integrations: [mdx(), sitemap()]
+//});
